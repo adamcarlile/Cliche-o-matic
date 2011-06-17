e6be325ee44969d7bc45dcc1767f612478c63fa6
Adam Carlile <adam@benchmedia.co.uk>
2011-06-17 15:11:18 +0100

    added javascrip reload

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,17 @@
+function getHrefContent(element, container, result) {
+	href = element.attr('href');
+	$(container).fadeOut(function(){
+		$(container).load(href + ' ' + result, function(){
+			$(container).fadeIn()
+		});
+	})
+}
+
 $(document).ready(function(){
 	$("header h2").fitText(0.5);
+	
+	$('a.ajax').live('click', function(e){
+		e.preventDefault();
+		getHrefContent($(this), '#content', '#result')
+	})
 })
